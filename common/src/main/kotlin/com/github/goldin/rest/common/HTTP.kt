b286eaf2629b5ca6e1673132c0f17e906aab8e92
Evgeny Goldin <evgenyg@gmail.com>
2012-07-17 01:24:11 +0200

    Switched to SLF4J (compile scope) and Logback (test scope).

--- common/src/main/kotlin/com/github/goldin/rest/common/HTTP.kt
+++ common/src/main/kotlin/com/github/goldin/rest/common/HTTP.kt
@@ -7,24 +7,32 @@ import com.google.api.client.http.GenericUrl
 import com.google.api.client.http.HttpRequestFactory
 import com.google.api.client.http.HttpResponseException
 import com.google.api.client.http.LowLevelHttpResponse
-import java.util.logging.Logger
-import java.util.logging.Level
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 
 class HTTP
 {
     final val factory : HttpRequestFactory = NetHttpTransport().createRequestFactory()!!
-    final val log     : Logger             = Logger.getLogger( "HTTP" )!!;
+    final val log     : Logger             = LoggerFactory.getLogger( "HTTP" )!!
 
 
     fun getResponse( url : String ): HttpResponse
     {
-        log.info( "GET: [${ url }]" )
+        if ( log.isDebugEnabled())
+        {
+            log.debug( "GET: [${ url }]" )
+        }
+
         val t        = System.currentTimeMillis()
         val request  = factory.buildGetRequest( GenericUrl( url ))!!
         val response = request.setThrowExceptionOnExecuteError( false )!!.execute()!!
 
-        log.info( "GET: [${ url }] - [${ response.getStatusCode()}] (${ System.currentTimeMillis() - t } ms)" )
+        if ( log.isDebugEnabled())
+        {
+            log.debug( "GET: [${ url }] - [${ response.getStatusCode()}] (${ System.currentTimeMillis() - t } ms)" )
+        }
+
         return response
     }
 
