5e06835ba3e48a370720e3a202b999a4fca88ced
Evgeny Goldin <evgenyg@gmail.com>
2012-07-16 12:23:11 +0200

    Readme update, HTTP usage added, IDEA files update

--- README.txt
+++ README.txt
@@ -1,2 +1,8 @@
 
-Java REST API for YouTrack, TeamCity, and Artifactory.
+* Java REST API for YouTrack, TeamCity, and Artifactory.
+* Use IDEA 11:       http://download.jetbrains.com/idea/ideaIU-11.1.2.exe (Ultimate Edition)
+                     http://download.jetbrains.com/idea/ideaIC-11.1.2.exe (Community Edition)
+* Use Kotlin plugin: http://teamcity.jetbrains.com/viewLog.html?buildTypeId=bt345&buildId=lastSuccessful&guest=1&tab=artifacts
+                     http://teamcity.jetbrains.com/guestAuth/repository/download/bt345/0.1.2858/kotlin-plugin-0.1.2858.zip
+                     (0.1.2858 built on July 16)
+
--- common/src/main/kotlin/com/github/goldin/rest/common/Hello.kt
+++ common/src/main/kotlin/com/github/goldin/rest/common/Hello.kt
@@ -1,6 +1,22 @@
 package com.github.goldin.rest.common
 
-fun main(args : Array<String>)
+import com.google.api.client.http.HttpResponse
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.http.HttpRequest
+import com.google.api.client.http.GenericUrl
+
+
+fun get( url : String ): String?
 {
-    println("Hello, world!")
+    val request  = NetHttpTransport().createRequestFactory()?.buildGetRequest( GenericUrl( url ))
+    val response = request?.execute()
+    return response?.parseAsString()
+}
+
+
+fun main( args : Array<String> )
+{
+    println( get( "https://twitter.com/hhariri" ))
+    println( get( "http://code.google.com/p/google-http-java-client/" ))
+    println( get( "http://code.google.com/" ))
 }
